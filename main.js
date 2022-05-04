d1902c951c5ff27e88ce293084f5304d79a88052
Molerog <102958319+Molerog@users.noreply.github.com>
2022-05-04 16:59:32 +0200

    incluida funcionalidad Javascript

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+const welcomeContainer = document.getElementById("welcomeContainer");
+const questionContainer = document.getElementById("questionContainer");
+const resultContainer = document.getElementById("resultContainer");
+const startButton = document.getElementById("start-btn");
+const questionPrint = document.getElementById("questionPrint");
+const answerButtonPrint = document.getElementById("answertButtonPrint");
+const nextButton = document.getElementById("next-btn");
+const restartButton = document.getElementById("restart-btn");
 
-
+let currentQuestionIndex;
 
 
 const questions = [
@@ -28,4 +36,73 @@ const questions = [
       ],
     },
   ];
+  
+
+  function setStatusClass(element, correct){
+    if (correct){
+      element.classList.add('correct');
+    } else {
+      element.classList.add('wrong');
+    }
+  }
+
+
+
+  function selectAnswer(){
+    Array.from(questionPrint.children).forEach(button => {
+     setStatusClass(button, button.dataset.correct) 
+    });
+    if (questions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide');
+    } else{
+      restartButton.classList.remove('hide');
+    }
+  }
+
+  function showQuestion(question){
+    questionPrint.innerText = question.question;
+    question.answers.forEach(answer => {
+      const button = document.createElement('button');
+      button.innerText = answer.text;
+      button.classList.add('btn');
+      if(answer.correct){
+        button.dataset.correct = true;
+      }
+      button.addEventListener('click',selectAnswer)
+      questionPrint.appendChild(button)      
+    });
+  }
+
+
+  function resetState() {
+    nextButton.classList.add('hide');
+    while (questionPrint.firstChild){
+      questionPrint.removeChild(questionPrint.firstChild)
+     }
+  }
+
+
+  function setNextQuestion(){
+    resetState()
+    showQuestion(questions[currentQuestionIndex])
+  }
+
+
+  function startGame(){
+    welcomeContainer.classList.add('hide')
+    currentQuestionIndex = 0
+    questionContainer.classList.remove('hide')
+    setNextQuestion()
+    }
+    
+    startButton.addEventListener('click',startGame)
+    nextButton.addEventListener('click',() =>{
+    currentQuestionIndex++;
+    setNextQuestion()
+  })
+
+  restartButton.addEventListener('click', startGame)  
+  
+  
+  
   
