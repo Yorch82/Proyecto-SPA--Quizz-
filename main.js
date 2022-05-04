70b736582cd7ecd6b0c24f07dd832260b6e1950e
Yorch82 <jcampo1982@outlook.com>
2022-05-04 15:11:44 +0200

    estructura html

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+
+const questions = [
+    {
+      question: "What is 2 + 2?",
+      answers: [
+        { text: "4", correct: true },
+        { text: "22", correct: false },
+      ],
+    },
+    {
+      question: "Is web development fun?",
+      answers: [
+        { text: "Kinda", correct: false },
+        { text: "YES!!!", correct: true },
+        { text: "Um no", correct: false },
+        { text: "IDK", correct: false },
+      ],
+    },
+    {
+      question: "What is 4 * 2?",
+      answers: [
+        { text: "6", correct: false },
+        { text: "8", correct: true },
+        { text: "Yes", correct: false },
+      ],
+    },
+  ];
+  
