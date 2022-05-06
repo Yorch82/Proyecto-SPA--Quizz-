3929470d70716e441e8caadd3dbc540099a2085b
Molerog <102958319+Molerog@users.noreply.github.com>
2022-05-06 12:36:00 +0200

    respuestas pintadas

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ const welcomeContainer = document.getElementById("welcomeContainer");
 const questionContainer = document.getElementById("questionContainer");
 const resultContainer = document.getElementById("resultContainer");
 const startButton = document.getElementById("start-btn");
-const questionPrint = document.getElementById("questionPrint");
-const answerButtonPrint = document.getElementById("answerButtonPrint");
 const nextButton = document.getElementById("next-btn");
 const restartButton = document.getElementById("restart-btn");
-let currentQuestionIndex;
+const questionPrint = document.getElementById("questionPrint")
+const answerButtonPrint1 = document.getElementById("text_button1");
+const answerButtonPrint2 = document.getElementById("text_button2");
+const answerButtonPrint3 = document.getElementById("text_button3");
+const answerButtonPrint4 = document.getElementById("text_button4");
 
 
-  axios.get("https://opentdb.com/api.php?amount=10&type=multiple")    
-  .then((res) => {
-    let arrayQuestions = res.data.results
-    console.log(arrayQuestions)
-    startGame(arrayQuestions)
-  })      
-  .catch((err) => console.error(err));
+
+let currentQuestionIndex; 
+
+
+  
 
 
   function setStatusClass(element, correct){
@@ -40,42 +40,66 @@ let currentQuestionIndex;
   }
 
   function showQuestion(question){    
-    questionPrint.innerText = question.question;
-    question.answers.forEach(answer => {
-      const button = document.createElement('button');
-      button.innerText = answer.text;
-      button.classList.add('btn');
-      if(answer.correct){
-        button.dataset.correct = true;
-      }
-      button.addEventListener('click',selectAnswer)
-      answerButtonPrint.appendChild(button)      
-    });
+    questionPrint.innerHTML = `<h2>${question.question}</h2>`;
+    showAnswers(question)
+   
+    
+  
+    //   button.classList.add('btn');
+    //   if(answer.correct){
+    //     button.dataset.correct = true;
+    //   }
+    //   button.addEventListener('click',selectAnswer)
+    //   answerButtonPrint.appendChild(button)      
+    // });
   }
 
+  function showAnswers(question){
+    let correctAnswer = arrayAnswers.correct_answer;
+    let incorrectAnswer = arrayAnswers.incorrect_answers;
+    incorrectAnswer.push(correctAnswer);
+    allAnswers = incorrectAnswer.sort();
+    answerButtonPrint1.innerHTML = `${arrayAnswers[0]}`;
+    answerButtonPrint2.innerHTML = `${arrayAnswers[1]}`
+    answerButtonPrint3.innerHTML = `${arrayAnswers[2]}`
+    answerButtonPrint4.innerHTML = `${arrayAnswers[3]}`
+    //   button.classList.add('btn');
+    //   if(answer.correct){
+    //     button.dataset.correct = true;
+    //   }
+    //   button.addEventListener('click',selectAnswer)
+    //   answerButtonPrint.appendChild(button)      
+    // });
+  }
 
   function resetState() {
     nextButton.classList.add('hide');
-    while (answerButtonPrint.firstChild){
-      answerButtonPrint.removeChild(answerButtonPrint.firstChild)
-     }
+    // while (answerButtonPrint.firstChild){
+    //   answerButtonPrint.removeChild(answerButtonPrint.firstChild)
+    //  }
   }
 
 
   function setNextQuestion(arrayQuestions){
     resetState()
+    currentQuestionIndex = 0
     showQuestion(arrayQuestions[currentQuestionIndex])
   }
 
 
-  function startGame(arrayQuestions){
+  function startGame(){
+    axios.get("https://opentdb.com/api.php?amount=10&type=multiple")    
+  .then((res) => {
+    let arrayQuestions = res.data.results
     welcomeContainer.classList.add('hide')
-    currentQuestionIndex = 0
     questionContainer.classList.remove('hide')
     setNextQuestion(arrayQuestions)
+  })      
+  .catch((err) => console.error(err));
+    
     }
     
-  startButton.addEventListener('click',startGame)
+    startButton.addEventListener('click',startGame)
     nextButton.addEventListener('click',() =>{
     currentQuestionIndex++;
     setNextQuestion()
