741623bf64e0bca2e28c16d2ead05437c028a2be
yorch82 <jcampo1982@outlook.com>
2022-05-08 17:40:34 +0200

    shake y estructura modal

--- main.js
+++ main.js
@@ -14,6 +14,34 @@ const totalQuestions = 10;
 let correctAnswers = 0;
 let currentQuestionIndex;
 
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn2 = document.getElementById("viewStats");
+
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn2.addEventListener('click',function() {
+  modal.style.display = "block";
+})
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 function setStatusClass(element, correct){
   if (correct){     
     element.classList.add('correct');      
